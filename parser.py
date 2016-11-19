2895c0640dbbb9699d9890b870e5701d8a8a47a6
Julia Huang <julia_h@mit.edu>
2016-11-19 18:14:39 -0500

    yay functional parser

--- parser.py
+++ parser.py
@@ -2,16 +2,16 @@ import csv, itertools
 
 def parse(file): 
 	with open(file, 'rb') as csvfile:
-		reader, reader2 = itertools.tee(csv.reader(csvfile, delimiter=' ', quotechar='|'))
-		data = [set() for j in next(reader2)]
-		columns = len(data)
+		reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+		data = []
 		count = 0
-		print data
 		for row in reader:
 			count += 1
-			for j in row:
-				
-				data[j].add(row[j])
+			array = row[0].split(',')
+			for j in range(len(array)):
+				if (len(data) <= j):
+					data.append([])
+				data[j].append(array[j])
    	print data
 
 parse('data.csv');
